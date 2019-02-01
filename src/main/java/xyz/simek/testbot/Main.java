2d0996b5efb8904256822330504a8f1a5794cf29
Jan Šimek <jan.simek@cgi.com>
2019-02-01 10:28:54 +0100

    generate intent models

--- src/main/java/xyz/simek/testbot/Main.java
+++ src/main/java/xyz/simek/testbot/Main.java
@@ -8,8 +8,15 @@ import com.rabidgremlin.mutters.core.session.Session;
 public class Main {
     public static void main(String[] args) throws BotException {
 
-        TaxiInkBot bot = new TaxiInkBot(new TaxiInkBotConfiguration());
-        BotResponse response = bot.respond(new Session(), new Context(), "I would like to order a cab at 1323 Street");
-        System.out.println("Bot says: " + response.getResponse());
+        Session session = new Session();
+        Context context = new Context();
+        TaxiInkBot taxiBot = new TaxiInkBot(new TaxiInkBotConfiguration());
+        BotResponse response = taxiBot.respond(session, context, "Where is my ride ?");
+
+//        TaxiInkBot bot = new TaxiInkBot(new TaxiInkBotConfiguration());
+//        BotResponse response = bot.respond(session, context, "Order me a taxi"); // at 1323 Street");
+//        System.out.println("Bot says: " + response.getResponse());
+//        response = bot.respond(session, context, "136 River Road");
+//        System.out.println("Bot says: " + response.getResponse());
     }
 }
--- src/main/java/xyz/simek/testbot/TaxiInkBotConfiguration.java
+++ src/main/java/xyz/simek/testbot/TaxiInkBotConfiguration.java
@@ -1,11 +1,15 @@
 package xyz.simek.testbot;
 
 import com.bladecoder.ink.runtime.Story;
-import com.rabidgremlin.mutters.bot.ink.*;
+import com.rabidgremlin.mutters.bot.ink.CurrentResponse;
+import com.rabidgremlin.mutters.bot.ink.InkBotConfiguration;
+import com.rabidgremlin.mutters.bot.ink.InkBotFunction;
+import com.rabidgremlin.mutters.bot.ink.StoryUtils;
 import com.rabidgremlin.mutters.core.Intent;
 import com.rabidgremlin.mutters.core.IntentMatch;
 import com.rabidgremlin.mutters.core.IntentMatcher;
 import com.rabidgremlin.mutters.core.session.Session;
+import com.rabidgremlin.mutters.core.util.SessionUtils;
 import com.rabidgremlin.mutters.opennlp.intent.OpenNLPIntentMatcher;
 import com.rabidgremlin.mutters.opennlp.intent.OpenNLPTokenizer;
 import com.rabidgremlin.mutters.opennlp.ner.OpenNLPSlotMatcher;
@@ -15,19 +19,22 @@ import opennlp.tools.tokenize.WhitespaceTokenizer;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TaxiInkBotConfiguration implements InkBotConfiguration {
+public class TaxiInkBotConfiguration
+        implements InkBotConfiguration
+{
 
     @Override
-    public IntentMatcher getIntentMatcher() {
+    public IntentMatcher getIntentMatcher()
+    {
         // model was built with OpenNLP whitespace tokenizer
         OpenNLPTokenizer tokenizer = new OpenNLPTokenizer(WhitespaceTokenizer.INSTANCE);
 
-        // use OpenNLP NER for slot matching
+        // use Open NLP NER for slot matching
         OpenNLPSlotMatcher slotMatcher = new OpenNLPSlotMatcher(tokenizer);
         slotMatcher.addSlotModel("Address", "models/en-ner-address.bin");
 
         // create intent matcher
-        OpenNLPIntentMatcher matcher = new OpenNLPIntentMatcher("models/en-cat-taxi-intents.bin", tokenizer, slotMatcher, 0.5f, -1);
+        OpenNLPIntentMatcher matcher = new OpenNLPIntentMatcher("models/en-cat-taxi-intents.bin", tokenizer, slotMatcher);
 
         Intent intent = new Intent("OrderTaxi");
         intent.addSlot(new LiteralSlot("Address"));
@@ -56,32 +63,39 @@ public class TaxiInkBotConfiguration implements InkBotConfiguration {
     }
 
     @Override
-    public String getStoryJson() {
+    public String getStoryJson()
+    {
         return StoryUtils.loadStoryJsonFromClassPath("taxibot.ink.json");
     }
 
     @Override
-    public List<InkBotFunction> getInkFunctions() {
+    public List<InkBotFunction> getInkFunctions()
+    {
         List<InkBotFunction> functions = new ArrayList<>();
 
-        functions.add(new InkBotFunction() {
+        functions.add(new InkBotFunction()
+        {
             @Override
             public void execute(CurrentResponse currentResponse, Session session, IntentMatch intentMatch,
-                                Story story, String param) {
-                try {
-                    // generate a fake order number based on address for demo
+                                Story story, String param)
+            {
+                try
+                {
                     story.getVariablesState().set("taxiNo",
                             Integer
                                     .toHexString(SessionUtils
                                             .getStringFromSlotOrSession(intentMatch, session, "address", "").hashCode())
                                     .substring(0, 4));
-                } catch (Exception e) {
+                }
+                catch (Exception e)
+                {
                     throw new RuntimeException("Unable to set taxi no", e);
                 }
             }
 
             @Override
-            public String getFunctionName() {
+            public String getFunctionName()
+            {
                 return "ORDER_TAXI";
             }
         });
@@ -90,24 +104,25 @@ public class TaxiInkBotConfiguration implements InkBotConfiguration {
     }
 
     @Override
-    public List<GlobalIntent> getGlobalIntents() {
-
+    public List<GlobalIntent> getGlobalIntents()
+    {
         List<GlobalIntent> globalIntents = new ArrayList<GlobalIntent>();
 
         globalIntents.add(new GlobalIntent("Stop", "stop"));
         globalIntents.add(new GlobalIntent("Help", "help"));
-        globalIntents.add(new GlobalIntent("OrderTaxi", "order_taxi"));
 
         return globalIntents;
     }
 
     @Override
-    public ConfusedKnot getConfusedKnot() {
+    public ConfusedKnot getConfusedKnot()
+    {
         return null;
     }
 
     @Override
-    public List<String> getDefaultResponses() {
+    public List<String> getDefaultResponses()
+    {
         return null;
     }
 
